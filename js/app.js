0484f42d5b4d686a4978d6cf66b96e31fa91074e
Rajmshah <raj.mshah2015@gmail.com>
2016-03-14 12:00:58 +0530

    making capital in table

--- js/app.js
+++ js/app.js
@@ -227,6 +227,27 @@ firstapp.directive('fancyboxBox', function($document) {
   };
 });
 
+firstapp.directive('capitalizeFirst', function ($parse) {
+   return {
+       require: 'ngModel',
+       link: function (scope, element, attrs, modelCtrl) {
+           var capitalize = function (inputValue) {
+               if (inputValue === undefined) {
+                   inputValue = '';
+               }
+                var capitalized = inputValue.charAt(0).toUpperCase() + inputValue.substring(1);
+               //var capitalized = inputValue.toUpperCase();
+               if (capitalized !== inputValue) {
+                   modelCtrl.$setViewValue(capitalized);
+                   modelCtrl.$render();
+               }
+               return capitalized;
+           }
+           modelCtrl.$parsers.push(capitalize);
+           capitalize($parse(attrs.ngModel)(scope)); // capitalize initial value
+       }
+   };
+});
 firstapp.directive('toggleOffCanvas', function($compile, $parse) {
   return {
     restrict: 'EA',
