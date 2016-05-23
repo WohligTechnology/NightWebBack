264275dab07eeeb8a5eb9b48aee7daed52deaa8e
TusharSachde <tushar@wohlig.com>
2016-05-23 18:38:24 +0530

    Try now

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,22 @@ var jsArray = [
     './bower_components/jquery/dist/jquery.min.js',
     './bower_components/flexslider/jquery.flexslider-min.js',
     './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-
     './bower_components/lodash/lodash.js',
+    './bower_components/tinymce-dist/tinymce.js',
+
     './bower_components/angular/angular.js',
     './bower_components/angular-animate/angular-animate.min.js',
     './bower_components/angular-flexslider/angular-flexslider.js',
     './bower_components/angular-sanitize/angular-sanitize.min.js',
-
     './bower_components/ui-router/release/angular-ui-router.min.js',
-
-
     './bower_components/angular-bootstrap/ui-bootstrap.min.js',
     './bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
     './bower_components/bootstrap-file-input/bootstrap.file-input.js',
-
     './bower_components/select2/dist/js/select2.js',
     './bower_components/angular-ui-select2/src/select2.js',
     './bower_components/angularjs-imageupload-directive/public/javascripts/imageupload.js',
     './bower_components/angular-toastr/dist/angular-toastr.tpls.js',
+    './bower_components/angular-ui-tinymce/src/tinymce.js',
 
     './js/app.js',
     './js/controllers.js',
